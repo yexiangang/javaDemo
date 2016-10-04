09e15ce3400e5857393fe13d59dfff4438418350
yexiangang <ye_xiangang@126.com>
2016-10-04 16:22:28 +0800

    create project

---
+++ java2DLineDemo/src/main/java/yexg/demo/java2D/line/SplitLine2D.java
@@ -0,0 +1,23 @@
+package yexg.demo.java2D.line;
+
+import javax.swing.JFrame;
+
+public class SplitLine2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("2D Demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,600);
+		frame.setVisible(true);
+	}
+	
+	public void runDemo(){
+		
+	}
+	
+	public void drawLine(){
+		
+	}
+
+}
